7d853cd178e0ece14fd51ad9901d91e8e2b8e3f5
EdieBishop <2496273B@student.gla.ac.uk>
2019-11-18 14:42:39 +0000

    Minor formatting changes

--- 0301_run_SIS.R
+++ 0301_run_SIS.R
@@ -20,8 +20,9 @@ source("0301_plot_helper.R")
 source("0301_step_SIS.R")
 
 #' ### Load packages
-library(kableExtra) # Allows us to build tables in the html report 
-library(knitr)
+library(kableExtra, warn.conflicts = FALSE) # Allows us to build tables in the html report 
+library(knitr, warn.conflicts = FALSE)
+library(tidyverse, warn.conflicts = FALSE) # Used to build a tibble 
 
 #' ### Define parameters 
 
@@ -31,7 +32,7 @@ num.cattle <- 100
 initial.infected <- 2
 initial.susceptible <- num.cattle - initial.infected 
 
-#' Next we define our dataframe, herd.df.x, which is a dataframe contianing two columns: number of susceptible 
+#' Next we define our dataframes, herd.df.x, contian two columns: number of susceptible 
 #' cattle and number of infected cattle. Each dataframe will correspond to a differnt rate of E. coli transmission. 
 herd.df.a <- data.frame(SUSCEPTIBLES = initial.susceptible,
                       INFECTED = initial.infected)
@@ -63,7 +64,7 @@ timesteps <- seq(from = start.time + 1, to = end.time)
 #' transmission rates. 
 
 for(new.time in timesteps) {
-  ## Calculate the changes to the herd using or steo_deterministic_SIS() function
+  ## Calculate the changes to the herd using or step_deterministic_SIS() function
   updated.population <- step_deterministic_SIS(latest=tail(herd.df.a, 1),
                                    transmission.rate = ecoli.transmission.a,
                                    recovery.rate = ecoli.recovery)
@@ -72,7 +73,7 @@ for(new.time in timesteps) {
   herd.df.a <- rbind(herd.df.a, updated.population)
 }
 
-#' We need to add another column (time) to our population.df dataframe, so that we can make a plot of 
+#' We need to add another column (time) to our herd.df dataframe, so that we can make a plot of 
 #' population againsts time. We do this by using c() to add start.time and timesteps to the dataframe 
 #' in a newly defined column below: 
 herd.df.a$time <- c(start.time, timesteps)
@@ -117,21 +118,42 @@ herd.df.d$time <- c(start.time, timesteps)
 
 #' Then we can use our plot_population function to plot the herdn.df dataframe. 
 plot_populations(herd.df.a, col = c("green", "red"))
-#' **Figure 1** Plot of E.coli outbreak where the green line representst susceptible cattle and the red line 
+#' **Figure 1** Plot of E.coli outbreak where the green line represents susceptible cattle and the red line 
 #' infected cattle, assuming a transmission rate of 1/2 and a recovery rate of 1/3, over 50 weeks. 
 plot_populations(herd.df.b, col = c("green", "red"))
-#' **Figure 2** Plot of E.coli outbreak where the green line representst susceptible cattle and the red line 
+#' **Figure 2** Plot of E.coli outbreak where the green line represents susceptible cattle and the red line 
 #' infected cattle, assuming a transmission rate of 2/3 and a recovery rate of 1/3, over 50 weeks. 
 plot_populations(herd.df.c, col = c("green", "red"))
-#' **Figure 3** Plot of E.coli outbreak where the green line representst susceptible cattle and the red line 
+#' **Figure 3** Plot of E.coli outbreak where the green line represents susceptible cattle and the red line 
 #' infected cattle, assuming a transmission rate of 1 and a recovery rate of 1/3, over 50 weeks. 
 plot_populations(herd.df.d, col = c("green", "red"))
-#' **Figure 4** Plot of E.coli outbreak where the green line representst susceptible cattle and the red line 
+#' **Figure 4** Plot of E.coli outbreak where the green line represents susceptible cattle and the red line 
 #' infected cattle, assuming a transmission rate of 4/3 and a recovery rate of 1/3, over 50 weeks. 
+#' 
+#' ------------------------------------------------------------------------------------------------------
+#' 
 
-table.df <- data.frame("Transmission.rates" = c(ecoli.transmission.a, ecoli.transmission.b, ecoli.transmission.c, ecoli.transmission.d),
-                       "Ro" = c("Transmission.rates" / (1/3)), 
-                       "Equilibrium.susceptibility" = 
-                       
+#'  
+#' The table created below shows the transmission rates, the Basic Reproduction Number (Ro = transmission 
+#' rate divided by the recovery rate), the proportion of susceptible individuals at equilibrium,
+#' and in inverse of Ro. 
+table <- tibble("Transmission rates" = c("1/2", "2/3", "1", "4/3"),
+                "Ro" = c("1.5", "2", "3", "4"),
+                "Proportion susceptible at equilibrium (S/N)" = c("0.66", "0.5", "0.33", "0.25"),
+               "1 / Ro" = c("2/3", "1/2", "1/3", "1/4"))
+
+kable(table) %>%
+  kable_styling(c("striped", "condensed"), full_width = F) %>%
+  row_spec(0) %>%
+  column_spec(1:4, width = "3cm")
+
+#' ### Conclusion
+#' 
+#' From the graphs created above, we can see that the in this deterministic disease model, where animals 
+#' which recover from the disease return to the infected population, transmission rate will directly 
+#' influence the proportion of infected and susceptible imdividuals at equilibrium. 
+#' Specifically, the proportion of the population which are susceptible at equilibrium is the same as 
+#' the value of 1/Ro. This is demonstrated in the table above. 
+#'   
 
 
